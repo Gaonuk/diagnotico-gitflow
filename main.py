9755c50da15467e2ffa768da2dc78295cd8fafe7
Gaonuk <rodrigo.gaona@uc.cl>
2022-08-15 16:08:10 -0400

    :recycle: Refactor data loading and add main function

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import json
 import heapq
-file = open("data.json", "r")
 
-lines = file.readlines()
+def load_data():
+    file = open("data.json", "r")
+    lines = file.readlines()
+    data = []
+    for line in lines:
+        data.append(json.loads(line))
+    file.close()
+    return data
 
-data = []
-for line in lines:
-    data.append(json.loads(line))
-
-file.close()
-
-def get_top_10_retweeted():
+def get_top_10_retweeted(data):
     top_10 = heapq.nlargest(10, data, key=lambda x:x["retweetCount"])
     return top_10
 
 def main():
-    pass
+    data = load_data()
+    result = get_top_10_retweeted(data)
+    print(result)
+
+if __name__ == "__main__":
+    main()
+    
